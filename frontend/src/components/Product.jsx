7316f203bd52ac50803a5f415265991a2ea29961
ahmad2shadab <ahmad2shadab@gmail.com>
2024-05-04 02:38:16 +0530

    React router bootstrap

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,19 +1,22 @@
 // import bootstrap
 import { Card } from 'react-bootstrap';
+// import link
+import { Link } from 'react-router-dom';
+
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.img} variant="top" />
-      </a>
+      </Link>
 
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         <Card.Body as="h3">${product.price}</Card.Body>
       </Card.Body>
