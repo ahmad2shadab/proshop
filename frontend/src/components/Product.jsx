8a63a263778777edb6614532bcae6543ee938f4d
ahmad2shadab <ahmad2shadab@gmail.com>
2024-05-05 00:38:01 +0530

    Rating component added & product image fixed

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -2,21 +2,28 @@
 import { Card } from 'react-bootstrap';
 // import link
 import { Link } from 'react-router-dom';
-
+import Rating from './Rating';
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.img} variant="top" />
+        <Card.Img src={product.image} variant="top" />
       </Link>
 
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
+          <Card.Title as="div" className="product-title">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
 
         <Card.Body as="h3">${product.price}</Card.Body>
       </Card.Body>
