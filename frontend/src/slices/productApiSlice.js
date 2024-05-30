be4038f530659c6dbbdc76e87e3956377436eef7
ahmad2shadab <ahmad2shadab@gmail.com>
2024-05-30 23:48:33 +0530

    added API,cart and productSlice

---
+++ frontend/src/slices/productApiSlice.js
@@ -0,0 +1,20 @@
+import { PRODUCTS_URL } from '../constants';
+import { apiSlice } from './apiSlice';
+
+export const productApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => ({
+        url: PRODUCTS_URL,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    getProductsDetails: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+      }),
+    }),
+  }),
+});
+
+export const { useGetProductsQuery,useGetProductsDetailsQuery } = productApiSlice;
