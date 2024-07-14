89ae1ea938911694346cc99748a5b3acde1450bc
ahmad2shadab <ahmad2shadab@gmail.com>
2024-07-15 00:44:38 +0530

    Checkout steps

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import FormContainer from '../components/FormContainer';
 import { saveShippingAddress } from '../slices/cartSlice';
+import CheckoutSteps from '../components/CheckoutSteps';
 
 const ShippingScreen = () => {
   const dispatch = useDispatch(); // Add this line to get the dispatch function
@@ -27,6 +28,8 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
+<CheckoutSteps step1 step2/>
+
       <h1>Shipping</h1>
 
       <Form onSubmit={submitHandler}>
